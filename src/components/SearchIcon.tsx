a78e7a1a4430f9d92f6c8a4515adf72284b0e044
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2021-01-19 21:21:13 -0800

    refactor search icon

---
+++ src/components/SearchIcon.tsx
@@ -0,0 +1,23 @@
+import { h } from "preact";
+
+const SearchIcon = ({ class: className }: { class?: string }) => {
+	return (
+		<svg
+			class={className}
+			width="32"
+			height="32"
+			viewBox="0 0 32 32"
+			fill="none"
+		>
+			<path
+				d="M28 28L20 20L28 28ZM22.6667 13.3333C22.6667 18.488 18.488 22.6667 13.3333 22.6667C8.17868 22.6667 4 18.488 4 13.3333C4 8.17868 8.17868 4 13.3333 4C18.488 4 22.6667 8.17868 22.6667 13.3333Z"
+				stroke="#ACACAC"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+};
+
+export default SearchIcon;
